192340e5b42677cb73bc3c11035a564fd9ff1270
Mykhailo Rud <rudmm1996@gmail.com>
2020-04-05 04:07:02 +0300

    responsive first 5 pages

--- hosting/assets/js/main.js
+++ hosting/assets/js/main.js
@@ -54,7 +54,7 @@ function advantagesSlider(){
         infinite: true,
         slidesToShow:2,
         slidesToScroll: 2,
-        //autoplay: true,
+        autoplay: true,
         autoplaySpeed: 3000,
         dots: true,
         appendDots: jQuery('.advantages-slider-dots'),
@@ -78,7 +78,7 @@ function reviewsSlider(){
         infinite:true,
         slidesToShow: 3,
         slidesToScroll: 1,
-        //autoplay: true,
+        autoplay: true,
         autoplaySpeed: 2000,
         arrows: false,
         dots: true,
@@ -131,7 +131,27 @@ function dataSlider(){
         focusOnSelect: true,
         arrows: false,
         dots: true,
-        appendDots: jQuery('.dots-data-slider')
+        appendDots: jQuery('.dots-data-slider'),
+        responsive: [
+            {
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 5
+                }
+            },
+            {
+                breakpoint: 767,
+                settings: {
+                    slidesToShow: 3
+                }
+            },
+            {
+                breakpoint: 550,
+                settings: {
+                    slidesToShow: 2
+                }
+            }
+        ]
     });
 }
 
